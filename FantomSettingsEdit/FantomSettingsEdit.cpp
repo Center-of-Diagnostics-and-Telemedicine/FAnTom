0d132e113b9148d753777889f02e49993d023255
kulberg <kulberg@yandex.ru>
2020-03-27 18:45:49 +0300

    Сделана зачаточная версия редактора настроек с сохранением в реестр

--- FantomSettingsEdit/FantomSettingsEdit.cpp
+++ FantomSettingsEdit/FantomSettingsEdit.cpp
@@ -1,8 +1,60 @@
 #include "pre.h"
 #include "FantomSettingsEdit.h"
+#include <Sources/XRAD/GUI/DynamicDialog.h>
+#include <QtCore/QSettings.h>
+//#include <QtCore/QCoreApplication.h>
+
+XRAD_USING
+
+struct WebServerSettings
+{
+	size_t	port = 5555;
+	wstring dicom_folder;
+};
+
+
+void		ExportSettings(WebServerSettings &wss)
+{
+	int	dummy_n = 0;
+	char	**argv = NULL;
+
+	QString	app_name("FAnTom WebServer");
+	QString	org_name("RPCMR");
+
+	QSettings	settings(QSettings::NativeFormat, QSettings::UserScope, org_name, app_name);
+	settings.setValue("Port", wss.port);
+	settings.setValue("Dicom folder", convert_to_string(wss.dicom_folder).c_str());
+
+	// все сохраняется в ветку реестра
+	//HKEY_CURRENT_USER\Software\RPCMR\FAnTom WebServer
+	// Наладить чтение оттуда, пополнить все параметры
+}
 
 int xrad::xrad_main(int,char * * const)
 {
-	double a = GetFloating("Введите число", 0, -1, 1);
+	try
+	{
+		auto	dialog = DynamicDialog::OKCancelDialog::Create(L"FAnTom web server settings");
+		WebServerSettings	wss;
+	
+//		wstring	some_string;
+
+		dialog->CreateControl<DynamicDialog::ValueDirectoryReadEdit>(L"Каталог с исследованиями для разметки", SavedGUIValue(&wss.dicom_folder), DynamicDialog::Layout::Vertical);
+//		dialog->CreateControl<DynamicDialog::StringEdit>(L"какая-то строковая настройка", SavedGUIValue(some_string));
+		dialog->CreateControl<DynamicDialog::ValueNumberEdit<size_t>>(L"Port", SavedGUIValue(&wss.port), 1024, 65535);
+
+//		dialog->AddControl(FoldersLayout(settings.target_folder, settings.source_folder/*, source_names_list*/));
+
+		dialog->Show();
+
+		ExportSettings(wss);
+
+
+
+	}
+	catch(...)
+	{
+
+	}
 	return 0;
 }
