0cab9069ff44b96263d37e8ede9b96d8e6fd9c7a
sprokudaylo <s.prokudaylo@npcmr.ru>
2020-03-30 19:08:40 +0300

    последние изменеия

--- FantomSettingsEdit/FantomSettingsEdit.cpp
+++ FantomSettingsEdit/FantomSettingsEdit.cpp
@@ -2,9 +2,12 @@
 #include "FantomSettingsEdit.h"
 #include <Sources/XRAD/GUI/DynamicDialog.h>
 #include <QtCore/QSettings.h>
+#include <iostream>
 //#include <QtCore/QCoreApplication.h>
 
 XRAD_USING
+
+using namespace std;
 
 struct WebServerSettings
 {
@@ -30,24 +33,54 @@ void		ExportSettings(WebServerSettings &wss)
 	// Наладить чтение оттуда, пополнить все параметры
 }
 
+
+void  ImportSettngs(WebServerSettings &wss)
+{
+	QSettings settings("HKEY_CURRENT_USER\\Software\\RPCMR\\FAnTom WebServer",
+		QSettings::NativeFormat);
+
+	size_t tmpPort;
+	tmpPort = settings.value("Port",     0).toInt();
+
+	QString tmpFolder = settings.value("Dicom folder", "C:").toString();
+
+	cout << tmpFolder.toLocal8Bit().constData() << endl;
+
+	wss.port = tmpPort;
+}
+
 int xrad::xrad_main(int,char * * const)
 {
 	try
 	{
 		auto	dialog = DynamicDialog::OKCancelDialog::Create(L"FAnTom web server settings");
 		WebServerSettings	wss;
+
+		ImportSettngs(wss);
 	
-//		wstring	some_string;
+		wstring	some_string;// = L"aaa";
 
 		dialog->CreateControl<DynamicDialog::ValueDirectoryReadEdit>(L"Каталог с исследованиями для разметки", SavedGUIValue(&wss.dicom_folder), DynamicDialog::Layout::Vertical);
-//		dialog->CreateControl<DynamicDialog::StringEdit>(L"какая-то строковая настройка", SavedGUIValue(some_string));
+	//	dialog->CreateControl<DynamicDialog::ValueDirectoryReadEdit>(L"Каталог с исследованиями для разметки", SavedGUIValue(&some_string), DynamicDialog::Layout::Vertical);
+	//	dialog->CreateControl<DynamicDialog::ValueDirectoryReadEdit>(L"Каталог с исследованиями для разметки", &some_string, DynamicDialog::Layout::Vertical);
+	//	dialog->CreateControl<DynamicDialog::StringEdit>(L"какая-то строковая настройка", some_string);
+
+		cout << wss.port << endl;
+		cout << &some_string << endl;
+
 		dialog->CreateControl<DynamicDialog::ValueNumberEdit<size_t>>(L"Port", SavedGUIValue(&wss.port), 1024, 65535);
+
+
 
 //		dialog->AddControl(FoldersLayout(settings.target_folder, settings.source_folder/*, source_names_list*/));
 
 		dialog->Show();
 
+		wcout << wss.dicom_folder << endl;
+
 		ExportSettings(wss);
+
+		Pause();
 
 
 
