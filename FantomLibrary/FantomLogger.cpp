8e839b5c7a5ebecf146bf727846a51624562c2f5
kulberg <kulberg@yandex.ru>
2020-04-30 00:41:36 +0300

    м Logger output fixed

--- FantomLibrary/FantomLogger.cpp
+++ FantomLibrary/FantomLogger.cpp
@@ -16,7 +16,7 @@ size_t	fantom_logger::counter = 0;
 fantom_logger::fantom_logger(const string &in_function_name) : m_id(counter++), m_function_name(in_function_name)
 {
 	m_finished_ok = false;
-	printf("----------'\n%s' (id=%zu) started", m_function_name.c_str(), m_id);
+	printf("\n----------'%s' (id=%zu) started", m_function_name.c_str(), m_id);
 	fflush(stdout);
 }
 
@@ -24,11 +24,11 @@ fantom_logger::~fantom_logger()
 {
 	if(m_finished_ok)
 	{
-		printf("----------'\n%s' (id=%zu) finished. OK", m_function_name.c_str(), m_id);
+		printf("\n----------'%s' (id=%zu) finished. OK", m_function_name.c_str(), m_id);
 	}
 	else
 	{
-		printf("'----------\n%s' (id=%zu) finished with errors", m_function_name.c_str(), m_id);
+		printf("\n----------'%s' (id=%zu) finished with errors", m_function_name.c_str(), m_id);
 		ForceDebugBreak();
 	}
 	fflush(stdout);
